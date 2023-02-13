071869f24fd3a017ac32d61e6bcedfe12ada9c1e
poserko <kokolin2204@gmail.com>
2023-02-13 09:08:46 +0200

    Zadacha

---
+++ Mvc_Zad/Controllers/TipCallculatorController.cs
@@ -0,0 +1,26 @@
+﻿using Mvc_Zad.Modal;
+using Mvc_Zad.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvc_Zad.Controllers
+{
+    public class TipCallculatorController
+    {
+        private Tip tip;
+        private Display display;
+
+        public TipCallculatorController()
+        {
+            display = new Display();
+            tip = new Tip(display.Amount, display.Percent);
+            display.TipAmount = tip.CalculateTip();
+            display.Total = tip.CalculateTotal();
+            display.ShowTipAndTotal();
+        }
+
+    }
+}
---
+++ Mvc_Zad/Program.cs
@@ -0,0 +1,13 @@
+﻿using Mvc_Zad.Controllers;
+using System;
+
+namespace Mvc_Zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TipCallculatorController tipCallcullatorController = new TipCallculatorController();
+        }
+    }
+}
